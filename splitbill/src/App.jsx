7ada53f0411dced2ffffee5bcc7cd0b7247eb1c0
samade747 <samad.e747@gmail.com>
2024-02-21 01:41:42 +0500

    141pm

---
+++ splitbill/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+
+    samads
+
+    </> 
+}
+
+export default App
