3a14b263961cb1fb90e63fed2bdceb0324cd61df
samade747 <samad.e747@gmail.com>
2024-02-23 10:34:05 +0500

    1033am

--- splitbill/src/App.jsx
+++ splitbill/src/App.jsx
@@ -2,15 +2,35 @@ import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Cards } from './Components/Cards'
+
 
 function App() {
-  
+
+  const [addFriend, setAddFriend] = useState(false)
+  const [name, setName] = useState('')
+  const [totalBill, setTotalBill] = useState(0)
+  const [myExpense, setMyExpense] = useState(0)
+  const [userExpense, setUserExpense] = useState(0)
+  const [payingBill, setPayingBill] = useState(0)
+  const [cardsData, setCardsData] = useState([
+    {
+      userImg: 'https://avatars.githubusercontent.com/u/132301326?v=4',
+      userName: 'samad',
+      desc: 'we have do anything',
+      price: '919',
+      isActive: false
+    },
+  ])
   return (
-    <>
+    <div className='flex justify-center'>
+      <div className=''>
+          <Cards
 
-    samads
 
-    </> 
-  )
-  }
+/>
+      </div>
+    </div>
+ )
+ }
 export default App
--- splitbill/src/Components/Cards.jsx
+++ splitbill/src/Components/Cards.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react"
+
+
+
+
 export const Cards = ({ cardsData }) => {
+
+    const [SelectSingleUser, setSelectSingleUser] = useState(false)
 
     return(
         <div className="">
@@ -15,7 +22,7 @@ export const Cards = ({ cardsData }) => {
 
 }
 
-const UserCard = ({ }) => {
+const UserCard = ({  }) => {
     console.log(`checking cardsData ${cardsData}`)
     console.log(`checking index, ${index}`)
 
@@ -25,11 +32,46 @@ const UserCard = ({ }) => {
         // checking the convertcarddata then find by val, index
         const checking = convertCardData.find((val, index1) => index1 === index)
         console.log(`checking ${checking}`)
-
         
+        convertCardData[index].isActive = true
+        setCardsData(convertCardData)
+        localStorage.setItem('card', JSON.stringify({ ...check, index: index }))        
+        setName(checking.username)
+        setSelectSingleUser(true)
     }
+    return (
+        <div className="" style={{
+                backgroundColor: setSelectSingleUser ? "red" : "orange",
+                padding: "5px 10px"}}>
+            <div className="flex flex-center">
+                <img style={{width: '50px', height: "50px", borderRadius: "50%", marginRight: "30px" }}
+                src={singlecard.userImg}
+                alt="userImage"
+                />
+            <div>
+                <p style={{ fontSize: "17px", fontWeight: "600", marginBottom: "2px", marginTop: "2px" }}>
+                {singlecard.username}                
+                </p>
 
-
-
-
+                <p style={{
+                    fontWeight: "200",
+                    color: myExpense < userExpense ? "purple" : "red"
+                }}>
+                    {singlecard.desc}
+                </p>
+                </div>
+            </div>
+        
+        <button onClick={() => selectedUser(index)}
+        style={{
+            padding: "10px 20px",
+            backgroundColor: "red",
+            border: "none",
+            borderRadius: "7px",
+            cursor: 'pointer'
+        }}>
+            select
+        </button>
+        </div>
+    )
 }
